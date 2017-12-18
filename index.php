b20e8560c3c0dacff6cf1e317bd9d81142b5a63e
kimhoang1802 <34208693+kimhoang1802@users.noreply.github.com>
2017-12-19 01:53:00 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+﻿<!doctype html>
+<html>
+<head>
+  <title>Tool Up Avatar Clone Facebook</title>
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+  <script>
+    function DuySexilee(){
+      if($('#token').val() == ''){
+        alert('Không nhập token thì up cmm ah ??');
+      }else{
+        $('#result').html('Đang xử lí....');
+        $('#submit').addClass('btn btn-info').html('Đang Up Avatar...').attr('disabled','disabled');
+        $.post('progress.php',{token:$('#token').val()},function(result){$('#result').html(result);$('#submit').removeClass('btn btn-info').addClass('btn btn-primary').html('Auto Up Avatar').removeAttr('disabled'); $('#token').val('');});
+      }
+    }
+  </script>
+  <div class="col-md-12" id="form">
+    <div class="box box-primary">
+      <div class="box-header with-border">
+        <h3 class="box-title">Tool Auto Up Avatar Profile, Fanpage</h3>
+      </div>
+        <div class="box-body">   
+           <div class="form-group">
+            <label for="nd">Nhập List Token, Mỗi Token 1 dòng</label>
+            <textarea class="form-control" rows="15" name="token" id="token" placeholder="Nhập List Token" required></textarea>
+          </div>
+          <span class="h4" id="result"></span>
+        </div>
+        <div class="box-footer">
+          <button type="button" class="btn btn-primary" id="submit" onclick="DuySexilee()">Auto Up Avatar</button>
+        </div>
+    </div>
+  </div>
+</body>
+</html>
---
+++ progress.php
@@ -0,0 +1,18 @@
+﻿<script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<?php
+if(isset($_POST['token'])){
+	$token = explode("\n",$_POST['token']);
+	$list_anh = array('http://bestauto.pro/_pro/tools/avt/avt1.jpg','http://bestauto.pro/_pro/tools/avt/avt2.jpg','http://bestauto.pro/_pro/tools/avt/avt3.jpg','http://bestauto.pro/_pro/tools/avt/avt4.jpg','http://bestauto.pro/_pro/tools/avt/avt5.jpg','http://bestauto.pro/_pro/tools/avt/avt6.jpg','http://bestauto.pro/_pro/tools/avt/avt7.jpg'); // tkay njst njnk ank? và0 dax
+	$i = 0;
+	$c = count($token);
+	for(;$i<$c;){
+		$anh = trim($list_anh[array_rand($list_anh)]);
+		$t = trim($token[$i]);
+		echo "<script>$.getJSON('https://graph.fb.me/me/photos?access_token=$t&caption=beautiful&url=$anh&method=post', function(DuySex){ var id = DuySex.id; $.getJSON('https://graph.fb.me/me/picture?photo='+id+'&caption=hihi&access_token=$t&method=post&width=700&height=700',function(Duy){console.log('success id: '+ Duy.id);});});</script>"; // thích xử lí cho token die, live thì dùng .done() .fail()
+		++$i;
+	}
+	echo 'Thành công';
+}else{
+	echo 'Bien me may dj';
+}
+?>
